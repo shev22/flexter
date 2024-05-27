be7465c05006ced257576e9bc6c25c60b1c913b9
Francis <shevy2k2@yahoo.com>
2024-05-27 16:50:47 +0300

    prod

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -281,13 +281,13 @@
                 </a>
             </li>
             <li class="relative px-6 py-3">
-                       @if (Route::is('settings'))
+                       @if (Route::is('admin/settings'))
                 <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
                     aria-hidden="true">
                 </span>
                 @endIf
                 <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                     href="{{ route('settings') }}">
+                     href="{{ route('admin/settings') }}">
                     <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
                         stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                         <path
@@ -325,7 +325,7 @@
             <!--        <span class="ml-4">Buttons</span>-->
             <!--    </a>-->
             <!--</li>-->
-            <li class="relative px-6 py-3">
+            {{-- <li class="relative px-6 py-3">
                 <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                     href="modals.html">
                     <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
@@ -336,7 +336,7 @@
                     </svg>
                     <span class="ml-4">Modals</span>
                 </a>
-            </li>
+            </li> --}}
             {{-- <!--<li class="relative px-6 py-3">-->
             <!--    <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"-->
             <!--        href="tables.html">-->
